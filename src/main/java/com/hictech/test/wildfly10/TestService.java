d0a5ee98b6cfc5f96952d765bf0342ac15ef887e
Andrea Cardamone <Andrea@andrea-laptop>
2015-09-21 16:01:42 +0200

    Added configuration of transaction via API because the configuration does'nt work

--- src/main/java/com/hictech/test/wildfly10/TestService.java
+++ src/main/java/com/hictech/test/wildfly10/TestService.java
@@ -5,8 +5,17 @@ import javax.ejb.Stateless;
 
 import org.infinispan.AdvancedCache;
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.TransactionMode;
+import org.infinispan.transaction.lookup.GenericTransactionManagerLookup;
+import org.infinispan.util.concurrent.IsolationLevel;
 
 @Stateless
 public class TestService {
@@ -17,13 +26,15 @@ public class TestService {
 	public String test() {
 		String test;
 		try {
+			manager.defineConfiguration("default", conf());
+			
 			test  = "cache manager class: " + cls(manager) + "\n";
 
 			Cache<Object, Object> cache = manager.getCache();
 			test += "cache default class: " + cls(cache) + "\n";
 
 			AdvancedCache<Object, Object> advanced_cache = cache.getAdvancedCache();
-			test += "tx manager class:   " + advanced_cache.getTransactionManager() + "\n";
+			test += "tx manager class:    " + advanced_cache.getTransactionManager() + "\n";
 
 			Configuration configuration = cache.getCacheConfiguration();
 			test += "cache confifugration:\n" + configuration + "\n";
@@ -34,7 +45,28 @@ public class TestService {
 			throw new RuntimeException(e);
 		}
 	}
-
+	
+	private static Configuration conf() {
+		return new ConfigurationBuilder()
+			.clustering()
+				.cacheMode(CacheMode.LOCAL)
+				
+			.transaction()
+				.transactionMode(TransactionMode.TRANSACTIONAL)
+				.transactionManagerLookup(new GenericTransactionManagerLookup())
+				.autoCommit(false)
+				
+			.lockingMode(LockingMode.PESSIMISTIC)
+				.locking()
+				.isolationLevel(IsolationLevel.SERIALIZABLE)
+				
+			.persistence()
+				.passivation(false)
+				.addSingleFileStore()
+				.purgeOnStartup(true)
+		.build();
+	}
+	
 	private String cls(Object o) {
 		return o != null? o.getClass().toString() : "null";
 	}
